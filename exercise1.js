2789ee6d93f1d852cda89921fb1007f834b7732c
irashovdra <109846925+irashovdra@users.noreply.github.com>
2024-01-14 19:03:58 -0600

    ex 1

--- exercise1.js
+++ exercise1.js
@@ -1,3 +1,12 @@
-const names = ["Iryna", "Liza", "Halia", "Tania"];
+// const names = ["Iryna", "Liza", "Halia", "Tania"];
 
-names.forEach((name) => alert(`${name}`));
+// names.forEach((name) => alert(`${name}`));
+
+const users = ["Iryna", "Liza", "Halia", "Tania"];
+
+const getUserNames = users => {
+  users.forEach
+};
+
+console.log(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
